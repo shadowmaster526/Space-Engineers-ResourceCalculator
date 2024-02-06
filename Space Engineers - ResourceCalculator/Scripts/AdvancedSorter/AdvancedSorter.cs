634c4a09b21a169a6521bb3d70ba4f6a4dc41b5c
termi17385 <66066987+termi17385@users.noreply.github.com>
2024-02-07 02:05:38 +1100

    Created folders for each script
    Additions:
    + Airlock Script
    + Advanced sorter

---
+++ Space Engineers - ResourceCalculator/Scripts/AdvancedSorter/AdvancedSorter.cs
@@ -0,0 +1,111 @@
+﻿using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using Sandbox.ModAPI.Ingame;
+using VRageMath;
+using System;
+using static VRage.Profiler.MyProfilerBlock;
+
+namespace IngameScript.Scripts.AdvancedSorter
+{
+    internal class Program : MyGridProgram
+    {
+        // Will contain the main methods
+        class LCDContainer
+        {
+            const string PROGBLOCK = "MyObjectBuilder_MyProgrammableBlock";
+            const string CRYOCHAMBER = "MyObjectBuilder_CryoChamber";
+            const string PROJECTOR = "MyObjectBuilder_Projector";
+            const string COCKPIT = "MyObjectBuilder_Cockpit";
+
+            public IMyTextSurface LCDTextSurface => lcd as IMyTextSurface;
+
+            public IMyTextSurface LCDTextPanel
+            {
+                get
+                {
+                    string blockType = lcd.BlockDefinition.TypeIdString;
+                    IMyTextSurface temp = null;
+                    string type = CRYOCHAMBER;
+
+                    if (blockType.Contains(type))
+                    {
+                        IMyCryoChamber cryo = lcd as IMyCryoChamber;
+                        if (cryo != null)
+                        {
+                            temp = (lcd as IMyCryoChamber).GetSurface(0);
+                        }
+                    }
+
+                    return temp;
+                }
+            }
+
+            private readonly IMyTerminalBlock lcd;
+            private readonly MyGridProgram mgp;
+
+            public LCDContainer(IMyTerminalBlock lcd, MyGridProgram mgp)
+            {
+                this.lcd = lcd;
+                this.mgp = mgp;
+            }
+
+            internal void WriteText(string _text)
+            {
+                LCDTextSurface?.WriteText(_text);
+                LCDTextPanel?.WriteText(_text);
+            }
+
+            internal string GetBlockName() => lcd?.CustomName;
+        }
+
+        const string LCDPREFIX = "[cLCD]";
+
+        readonly List<LCDContainer> LCDs = new List<LCDContainer>();
+
+        public Program()
+        {
+            Echo("Starting up.....");
+
+            try
+            {
+                Runtime.UpdateFrequency = UpdateFrequency.Update10;
+                List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+                GridTerminalSystem?.GetBlocks(blocks);
+
+                LCDContainer newLCD;
+                for (int i = 0; i < blocks.Count; i++)
+                {
+                    if (blocks[i].CustomName.Contains(LCDPREFIX))
+                    {
+                        newLCD = new LCDContainer(blocks[i], this);
+                        LCDs?.Add(newLCD);
+                    }
+                }
+            } catch (Exception ex) { Echo($"Exception {ex.Message}"); }
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Main(string argument, UpdateType updateType)
+        {
+            string str = "";
+            for (int i = 0; i < LCDs.Count; i++)
+            {
+                LCDContainer lcd = LCDs[i];
+                string temp = lcd.GetBlockName() + '\n';
+                string lcdSStatus = lcd.LCDTextSurface == null ? "missing" : "good";
+                string lcdPStatus = lcd.LCDTextPanel == null ? "missing" : "good";
+                temp += $"TextSurface Status: {lcdSStatus}\nTextPanel Status: {lcdPStatus}\n\n";
+                str += temp;
+            }
+
+            for (int i = 0; i < LCDs.Count; i++)
+            {
+                LCDs[i].WriteText(str);
+            }
+        }
+    }
+}
---
+++ Space Engineers - ResourceCalculator/Scripts/Airlock/Airlock.cs
@@ -0,0 +1,194 @@
+﻿using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using Sandbox.ModAPI.Ingame;
+using VRageMath;
+using System;
+using static VRage.Profiler.MyProfilerBlock;
+using System.Linq;
+using System.Text;
+
+namespace IngameScript.Scripts.Airlock
+{
+    internal class Program : MyGridProgram
+    {
+        enum Status
+        {
+            Depressurized,
+            Pressurized,
+            Pressurizing,
+            Depressurizing
+        }
+
+        const string DOOROUTPREFIX  =  "[airdoorout]";
+        const string DOORINPREFIX   =  "[airdoorin]";
+        const string AIRVENTPREFIX  =  "[airvent]";
+        const string LCDPREFIX      =  "[airlcd]";
+
+        string Progression
+        {
+            get
+            {
+                if (airvent == null) return ProgressionBarAnimation();
+                progression = MathHelper.RoundToInt((airvent.GetOxygenLevel() * 10));
+                return ProgressionBarAnimation();
+            }
+        }
+
+        string[] progressionDial = { "[|]", "[/]", "[-]", "[\\]" };
+        int index = 0;
+
+        int progression = 0;
+        int maxProgression = 10;
+        bool reverse = false;
+
+        IMyTextSurface lcd;
+        IMyAirVent airvent;
+        IMyDoor doorOut;
+        IMyDoor doorIn;
+
+        Status currentStatus = Status.Depressurized;
+
+
+        public Program()
+        {
+            Echo("Starting Airlock Procedures....");
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+
+            List<IMyTextSurface> lcdBlocks = new List<IMyTextSurface>();
+            List<IMyAirVent> airventBlocks = new List<IMyAirVent>();
+            List<IMyDoor> doorBlocks = new List<IMyDoor>();
+            
+            GridTerminalSystem.GetBlocksOfType(lcdBlocks);
+            GridTerminalSystem.GetBlocksOfType(airventBlocks);
+            GridTerminalSystem.GetBlocksOfType(doorBlocks);
+
+            lcd = lcdBlocks.FirstOrDefault(n => (n as IMyTerminalBlock).CustomName.Contains(LCDPREFIX));
+            airvent = airventBlocks.FirstOrDefault(n => n.CustomName.Contains(AIRVENTPREFIX));
+            doorOut = doorBlocks.FirstOrDefault(n => n.CustomName.Contains(DOOROUTPREFIX));
+            doorIn = doorBlocks.FirstOrDefault(n => n.CustomName.Contains(DOORINPREFIX));
+
+            if (airvent.GetOxygenLevel() > 0)
+            {
+                currentStatus = Status.Pressurized;
+                doorOut.Enabled = false;
+            }
+            else
+            {
+                currentStatus = Status.Depressurized;
+                doorIn.Enabled = false;
+            }
+        }
+
+        public void Save()
+        {
+            
+        }
+
+        public void Main(string argument, UpdateType updateType)
+        {
+            if (argument.Contains("Cycle")) CycleAirlock();
+
+            Depressurisation();
+
+            Repressurisation();
+
+            AirlockDisplay();
+        }
+
+        private void CycleAirlock()
+        {
+            if(currentStatus == Status.Pressurized)
+                currentStatus = Status.Depressurizing;
+         
+            else if(currentStatus == Status.Depressurized)
+                currentStatus = Status.Pressurizing;
+        }
+
+        private void Repressurisation()
+        {
+            if (currentStatus == Status.Pressurizing)
+            {
+                doorOut.CloseDoor();
+                if (doorOut.OpenRatio == 0)
+                {
+                    // start depressure
+                    doorOut.Enabled = false;
+                    airvent.Depressurize = false;
+
+                    if (progression > 5)
+                    {
+                        currentStatus = Status.Pressurized;
+                        doorIn.Enabled = true;
+                        doorIn.OpenDoor();
+                    }
+                }
+            }
+        }
+
+        private void Depressurisation()
+        {
+            if (currentStatus == Status.Depressurizing)
+            {
+                doorIn.CloseDoor();
+                if (doorIn.OpenRatio == 0)
+                {
+                    // start depressure
+                    doorIn.Enabled = false;
+                    airvent.Depressurize = true;
+                    if (progression == 0)
+                    {
+                        currentStatus = Status.Depressurized;
+                        doorOut.Enabled = true;
+                        doorOut.OpenDoor();
+                    }
+                }
+            }
+        }
+
+        private void AirlockDisplay()
+        {
+            string str = "Airlock One\n\n";
+            str += $"Status: {currentStatus}\n\n";
+
+            if (currentStatus == Status.Pressurizing || currentStatus == Status.Depressurizing)
+                str += ProgressDialAnimation() + "\n\n";
+
+            str += "Door Out: Closed\n\n";
+            str += "Door In: Opened\n\n";
+            str += $"Pressure Level: {Progression}";
+            lcd?.WriteText(str);
+        }
+
+        private string ProgressionBarAnimation()
+        {
+            StringBuilder bar = new StringBuilder();
+            string progressionBar = "[";
+            bar.Append('=', progression);
+            bar.Append('-', (maxProgression - progression));
+            progressionBar += (bar.ToString() + "]");
+
+            //if (!reverse) progression++;
+            //else progression--;
+
+            //if (progression > maxProgression)
+            //{
+            //    progression = maxProgression;
+            //    reverse = true;
+            //}
+            //else if (progression < 0)
+            //{
+            //    progression = 0;
+            //    reverse = false;
+            //}
+            return progressionBar;
+        }
+        string ProgressDialAnimation()
+        {
+            string str = progressionDial[index];
+            index++;
+            if (index > progressionDial.Length - 1) index = 0;
+            return str;
+        }
+    }
+}
