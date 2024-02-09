fe588b6d2a3a6f43f81371cc7c709b5d0f87b85f
termi17385 <66066987+termi17385@users.noreply.github.com>
2024-02-09 20:14:12 +1100

    Updated script now uses an LCD panel for display

--- Space Engineers - ResourceCalculator/Scripts/Comms/CommsSystem.cs
+++ Space Engineers - ResourceCalculator/Scripts/Comms/CommsSystem.cs
@@ -11,6 +11,10 @@ namespace IngameScript.Scripts.Comms
 {
     internal class Program : MyGridProgram
     {
+        const string LCDPREFFIX = "[COMMS]";
+
+        IMyTextSurface lcdPanel = null;
+
         int runCount = 0;
         string broadcastTag = "MDK IGC EXAMPLE 1";
         IMyBroadcastListener myBroadcastListener;
@@ -20,6 +24,18 @@ namespace IngameScript.Scripts.Comms
             Echo("Broadcasting....");
             myBroadcastListener = IGC.RegisterBroadcastListener(broadcastTag);
             myBroadcastListener.SetMessageCallback(broadcastTag);
+
+            List<IMyTextSurface> lcds = new List<IMyTextSurface>();
+            GridTerminalSystem.GetBlocksOfType(lcds);
+
+            for (int i = 0; i < lcds.Count; i++)
+            {
+                if ((lcds[i] as IMyTerminalBlock).CustomName.Contains(LCDPREFFIX))
+                {
+                    lcdPanel = lcds[i];
+                    break;
+                }
+            }
         }
 
         public void Save()
@@ -58,10 +74,11 @@ namespace IngameScript.Scripts.Comms
                         if (myIGCMessage.Data is string)
                         {
                             string str = myIGCMessage.Data.ToString();
-                            Echo("Received IGC Public Message");
-                            Echo("Tag=" + myIGCMessage.Tag);
-                            Echo("Data=" + myIGCMessage.Data.ToString());
-                            Echo("Source=" + myIGCMessage.Source.ToString("X"));
+                            str += "\n\nReceived IGC Public Message";
+                            str += "\nTag=" + myIGCMessage.Tag;
+                            str += "\nData=" + myIGCMessage.Data.ToString();
+                            str += "\nSource=" + myIGCMessage.Source.ToString("X");
+                            lcdPanel.WriteText(str);
                         }
                         else // if(msg.Data is XXX)
                         {
